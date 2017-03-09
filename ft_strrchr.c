ca2be3c4c98088156de08d9dce20c7828a3e1b42
Eliot vanHeumen <Eliot@Eliots-MacBook-Air.local>
2017-03-09 12:41:55 -0800

    strnstr strrchr

---
+++ ft_strrchr.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: evanheum <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/01 18:52:30 by evanheum          #+#    #+#             */
+/*   Updated: 2017/03/09 12:41:06 by evanheum         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <string.h>
+#include <stdio.h>
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int len;
+	int i;
+
+	i = 0;
+	len = ft_strlen(char*)s;
+	while ( i =< len && s[len] != (char)c)
+		len--;
+	if (s[len] == (char)c)
+		return ((char*)&s[len]);
+	return (0);
